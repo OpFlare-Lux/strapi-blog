9b8ec5f63a45eb3be556cb400cc2b73798451092
Serhii Suhal <vio3273@gmail.com>
2024-10-22 20:09:21 +0200

    delete plugin

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,7 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  // 'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -9,4 +9,18 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'cdn.jsdelivr.net', 'https://maps.googleapis.com'],
+          'connect-src': ["'self'", 'https:', 'http:', 'data:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'http://localhost', 'https://maps.googleapis.com', 'https://maps.gstatic.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'http://localhost', 'https://maps.googleapis.com'],
+        },
+      },
+    },
+  }
 ];
--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,6 @@
-module.exports = () => ({});
+module.exports = () => ({
+    'editorjs-editor': {
+        enabled: true,
+        resolve: './src/plugins/strapi-plugin-react-editorjs'
+    },
+});
