aa5257861337dc2e9e29e36ec93e9f5be9193515
Serhii Suhal <vio3273@gmail.com>
2024-10-28 11:04:03 +0100

    add preview plugin

---
+++ src/admin/app.js
@@ -0,0 +1,41 @@
+import Preview from "./extensions/Preview";
+
+const config = {
+  locales: [
+    // 'ar',
+    // 'fr',
+    // 'cs',
+    // 'de',
+    // 'dk',
+    // 'es',
+    // 'he',
+    // 'id',
+    // 'it',
+    // 'ja',
+    // 'ko',
+    // 'ms',
+    // 'nl',
+    // 'no',
+    // 'pl',
+    // 'pt-BR',
+    // 'pt',
+    // 'ru',
+    // 'sk',
+    // 'sv',
+    // 'th',
+    // 'tr',
+    // 'uk',
+    // 'vi',
+    // 'zh-Hans',
+    // 'zh',
+  ],
+};
+
+const bootstrap = (app) => {
+  app.getPlugin('content-manager').apis.addEditViewSidePanel([Preview]);
+};
+
+export default {
+  config,
+  bootstrap,
+};
---
+++ src/admin/extensions/Preview/index.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { Button } from "@strapi/design-system";
+import { PanelComponent, PanelComponentProps } from '@strapi/content-manager/strapi-admin';
+
+const PreviewButton: PanelComponent = ({
+       activeTab,
+       collectionType,
+       document: currentRecord,
+       documentId,
+       meta,
+       model
+   }: PanelComponentProps) => {
+    const typeMapping = {
+        'api::article.article': 'BLOG',
+    };
+
+    const locale = currentRecord.locale || 'en';
+    const slug = currentRecord.slug || 'test';
+    const status = currentRecord.status || 'draft';
+
+    window.Clipboard = (function(window, document, navigator) {
+        var textArea,
+            copy;
+
+        function isOS() {
+            return navigator.userAgent.match(/ipad|iphone/i);
+        }
+
+        function createTextArea(text) {
+            textArea = document.createElement('textArea');
+            textArea.value = text;
+            document.body.appendChild(textArea);
+        }
+
+        function selectText() {
+            var range,
+                selection;
+
+            if (isOS()) {
+                range = document.createRange();
+                range.selectNodeContents(textArea);
+                selection = window.getSelection();
+                selection.removeAllRanges();
+                selection.addRange(range);
+                textArea.setSelectionRange(0, 999999);
+            } else {
+                textArea.select();
+            }
+        }
+
+        function copyToClipboard() {
+            document.execCommand('copy');
+            document.body.removeChild(textArea);
+        }
+
+        copy = function(text) {
+            createTextArea(text);
+            selectText();
+            copyToClipboard();
+        };
+
+        return {
+            copy: copy
+        };
+    })(window, document, navigator);
+
+    const handleLink = () => {
+        let link = `https://easybiz-blog.netlify.app/api/preview?secret=j8heapkqy4rdz6kudrvsc7ywpvfhrv022abyx5zgmuwpc1xv&type=${typeMapping[model]}&locale=${locale}&slug=${slug}`;
+        Clipboard.copy(link);
+        navigator.clipboard.writeText(link);
+        alert('Link copied to your clipboard');
+    };
+
+    return {
+        title: 'Actions',
+        content: <Button
+            variant="tertiary"
+            fullWidth
+            disabled={model !== 'api::article.article' || status !== 'draft'}
+            onClick={handleLink}
+        >
+            Preview
+        </Button>
+    }
+}
+
+export default PreviewButton;
